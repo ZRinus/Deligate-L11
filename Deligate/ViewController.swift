89af00108a373b5005222272b415add7bdfe7b3c
Rinat Zaripov <z_renat@inbox.ru>
2023-09-04 20:36:39 +0600

    Initial Commit

---
+++ Deligate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deligate
+//
+//  Created by Rinat Zaripov on 04.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
