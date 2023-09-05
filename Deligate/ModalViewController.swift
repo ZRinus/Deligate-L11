2bcd2c39471238d39c9ffc2e76fddfa2dd379edd
Rinat Zaripov <z_renat@inbox.ru>
2023-09-05 22:59:12 +0600

    first commit

---
+++ Deligate/ModalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ModalViewController.swift
+//  Deligate
+//
+//  Created by Rinat Zaripov on 04.09.2023.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
